ccd827c2e2196936ccf569fb543aa77178186f06
Thomas <thomas@craftacademy.se>
2019-11-19 15:55:39 +0100

    adds weather conditions

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,60 @@ const App = () => {
     const response = await axios.get(
       'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.063240/lat/59.334591/data.json'
     )
+    setWeatherData(response.data)
+
   }, [])
 
+  const getTemperature = () => {
+    if (Object.entries(weatherData).length > 0) {
+      let temperatureParam = weatherData.timeSeries[0].parameters.filter(param => param.name == 't')
+      let temperature = temperatureParam[0].values[0]
+      return temperature
+    }
+  }
+
+  const getCurrentConditions = () => {
+    if (Object.entries(weatherData).length > 0) {
+      let currentConditionsParam = weatherData.timeSeries[0].parameters.filter(param => param.name == 'Wsymb2')
+      // debugger
+      let currentConditions = currentConditionsParam[0].values[0]
+      const weatherValues = [
+        'Clear sky',
+        'Nearly clear sky',
+        'Variable cloudiness',
+        'Halfclear sky',
+        'Cloudy sky',
+        'Overcast',
+        'Fog',
+        'Light rain showers',
+        'Moderate rain showers',
+        'Heavy rain showers',
+        'Thunderstorm',
+        'Light sleet showers',
+        'Moderate sleet showers',
+        'Heavy sleet showers',
+        'Light snow showers',
+        'Moderate snow showers',
+        'Heavy snow showers',
+        'Light rain',
+        'Moderate rain',
+        'Heavy rain',
+        'Thunder',
+        'Light sleet',
+        'Moderate sleet',
+        'Heavy sleet',
+        'Light snowfall',
+        'Moderate snowfall',
+        'Heavy snowfall'
+      ]
+      return weatherValues[currentConditions - 1]
+    }
+  }
   return (
     <Container>
       <Header as="h2">SMHI Prognos för Stockholm</Header>
+      <p>Current temperature is {getTemperature()} &#8304;</p>
+      <p> {getCurrentConditions()}</p>
     </Container>
   )
 }
